0554c4372981938eb3c4d3596b3dcc0808aae331
Danny Tey <dannyongtey@gmail.com>
2020-05-09 18:41:18 +0800

    add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   get "/signup", to: "users#new"
   get "/help", to: "static_pages#help" #,name: 'help'
