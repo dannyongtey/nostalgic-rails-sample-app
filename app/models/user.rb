44aacca67778c23509cf4572f37aacdd069f5259
Danny Tey <dannyongtey@gmail.com>
2020-05-10 22:01:05 +0800

    add test for homepage feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,9 @@ class User < ApplicationRecord
     following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids}) or user_id = :user_id", user_id: id)
+    # Inner join
+    # part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    # Micropost.joins(user: :followers).where(part_of_feed, { id: id })
   end
 
   def follow(other_user)
